e363101e4d2d019463a538eccecf5bf0cd13d02b
Ana Garrido Diaz <anaigdx@gmail.com>
2026-03-17 17:47:29 +0100

    ex00: add functions

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -22,19 +22,68 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter &original) {
     return *this;
 }
 
+
+bool isChar(std::string s) {
+    if(s.length() != 1)
+        return false;
+    if(!std::isdigit(s[0]))
+        return true;
+    return false;
+}
+
+bool isInt(std::string s) {
+    if(s.empty())
+        return false;
+    
+    size_t i = 0;
+
+    if(s[i] == '+' || s[i] == '-') {
+        if(s.length() == 1)
+            return false;
+        i++;
+    }
+        
+    for(; i < s.length(); i++) {
+        if(!std::isdigit(s[i]))
+            return false;
+    }
+    return true;
+}
+
+bool isFloat(std::string s) {
+
+}
+
+bool isDouble(std::string s) {
+
+}
+
+
 void ScalarConverter::convert(std::string s) {
-    /* Detección: Analizar el string para ver si es un char, int, float o double.
+    double item;
 
-    Conversión Inicial: Pasar ese string a un tipo contenedor (normalmente un double o un long double es buena idea porque "caben" todos los demás).
+    // 1. DETECCIÓN Y RELLENADO
+    if (isChar(s)) {
+        item = static_cast<double>(s[0]);
+    }
+    else if (isInt(s)) {
+        item = std::atof(s.c_str()); // O usar strtod
+    }
+    else if (isFloat(s)) {
+        // Cuidado: hay que quitar la 'f' del final antes de convertir
+        item = std::atof(s.c_str());
+    }
+    else if (isDouble(s)) {
+        item = std::atof(s.c_str());
+    }
+    else {
+        //pseudo-literales como nan, +inf, etc.
+        // O dejas que atof los maneje si son estándar.
+    }
 
-    Gestión de Casos Especiales: Si el string es "nan", "+inf", etc., ya sabes que el char y el int van a imprimir "impossible".
-
-    El bloque de "Prints":
-
-    Plaintext
-
-    char:   [Aquí calculas y decides si imprimes el char, 'Non displayable' o 'impossible']
-    int:    [Aquí calculas si cabe en un int o es 'impossible']
-    float:  [Imprimes con un decimal .0f]
-    double: [Imprimes con un decimal .0] */
+    // 2. IMPRESIÓN CONTROLADA
+    printChar(item);
+    printInt(item);
+    printFloat(item);
+    printDouble(item);
 }
